2fe25f93595f2a6cd0aa0a9157e520a3d50a1f61
Divyansh Bhatnagar <75672653+divyansh13kietmca@users.noreply.github.com>
2021-05-18 18:01:32 +0530

    Add files via upload

---
+++ Constructor2.java
@@ -0,0 +1,14 @@
+
+public class Constructor2 {
+int count = 10;
+  void Constructor2() {
+	// TODO Auto-generated constructor stub
+	 System.out.println("Count = ");
+}
+  Constructor2(){
+	  System.out.println("hello");
+  }
+ public static void main(String[] args) {
+	Constructor2 con = new Constructor2();
+}
+}
